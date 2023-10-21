7ae06f46c5f92f1ebf98caad67bcdc0c38348f70
kanav-dev <120954683+kanav-dev@users.noreply.github.com>
2023-10-21 18:21:01 +0530

    Create data.js

---
+++ accordion/data.js
@@ -0,0 +1,34 @@
+const faqData = [
+  {
+    id: 1,
+    que: "What is Lorem Ipsum?",
+    ans:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+  },
+  {
+    id: 2,
+    que: "Why do we use it?",
+    ans:
+      "It is a long-established fact that a reader will be distracted by the readable content of a page when looking at its layout."
+  },
+  {
+    id: 3,
+    que: "Where can I get some?",
+    ans:
+      "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form."
+  },
+  {
+    id: 4,
+    que: "Is it free?",
+    ans:
+      "Yes, Lorem Ipsum is free to use and can be found on various websites and in software applications."
+  },
+  {
+    id: 5,
+    que: "How long has it been used?",
+    ans:
+      "Lorem Ipsum has been used as a standard placeholder text since the 16th century."
+  }
+];
+
+export default faqData;
